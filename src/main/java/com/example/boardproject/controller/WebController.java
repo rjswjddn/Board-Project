58b716f8404b2727fdca71d2683a0d9b9ff4d5de
김건우 <rjswjddn@daou.co.kr>
2023-07-18 09:32:36 +0900

    fragment, WebController

---
+++ src/main/java/com/example/boardproject/controller/WebController.java
@@ -0,0 +1,63 @@
+package com.example.boardproject.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/401")
+    public String error401(){
+        return "401";
+    }
+
+    @GetMapping("/404")
+    public String error404(){
+        return "404";
+    }
+    @GetMapping("/500")
+    public String error500(){
+        return "500";
+    }
+
+    @GetMapping("/charts")
+    public String charts(){
+        return "charts";
+    }
+
+    @GetMapping("/layout-sidenav-light")
+    public String layoutSidenavLight(){
+        return "layout-sidenav-light";
+    }
+
+    @GetMapping("/layout-static")
+    public String layoutStatic(){
+        return "layout-static";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/password")
+    public String password(){
+        return "password";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @GetMapping("/tables")
+    public String tables(){
+        return "tables";
+    }
+
+}
