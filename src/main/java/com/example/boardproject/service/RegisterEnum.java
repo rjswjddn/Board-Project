a1ab6e37d04c15a3d8265ce3d6da55394d2e854a
rjswjddn <96411818+rjswjddn@users.noreply.github.com>
2023-07-26 14:17:02 +0900

    Revert "코드리뷰"

--- src/main/java/com/example/boardproject/service/RegisterEnum.java
+++
@@ -1,6 +0,0 @@
-package com.example.boardproject.service;
-
-public enum RegisterEnum {
-    PASS,
-    ID_DUP,
-}
--- src/test/java/com/example/boardproject/BoardProjectApplicationTests.java
+++ src/test/java/com/example/boardproject/BoardProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BoardProjectApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
