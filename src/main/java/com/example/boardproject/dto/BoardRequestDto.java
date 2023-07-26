bf5fa28b491217f7f9c6083069dfa84adc3b12c9
rjswjddn <96411818+rjswjddn@users.noreply.github.com>
2023-07-26 14:28:40 +0900

    Revert "Revert "코드리뷰""

---
+++ src/main/java/com/example/boardproject/dto/BoardRequestDto.java
@@ -0,0 +1,84 @@
+package com.example.boardproject.dto;
+
+import com.example.boardproject.entity.BoardEntity;
+import jakarta.persistence.Column;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Transient;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BoardRequestDto {
+    private Long boardSeq;
+
+    private String boardTitle;
+
+    private String boardContent;
+
+    private String boardType;
+
+    private int commentCnt;
+
+    private int viewCnt;
+
+    private int likeCnt;
+
+    private Boolean imageYn = false;
+
+    private Boolean boardStatus = false;
+
+    @CreatedDate
+    private LocalDateTime boardCreatedDate;
+
+    @LastModifiedDate
+    private LocalDateTime boardUpdatedDate;
+
+    private Long userSeq;
+
+    @Transient
+    private String userId;
+
+    @Transient
+    private String imagePath;
+
+    @Transient
+    private String imageName;
+
+    //INSERT
+    public BoardEntity toEntity() {
+        return BoardEntity.builder()
+                .boardTitle(this.boardTitle)
+                .boardContent(this.boardContent)
+                .boardType(this.boardType)
+                .commentCnt(this.commentCnt)
+                .viewCnt(this.viewCnt)
+                .likeCnt(this.likeCnt)
+                .imageYn(this.imageYn)
+                .boardStatus(this.boardStatus)
+                .boardCreatedDate(this.boardCreatedDate)
+                .userSeq(this.userSeq)
+                .build();
+
+    }
+
+    public BoardRequestDto(BoardResponseDto boardResponseDto) {
+        this.boardSeq = boardResponseDto.getBoardSeq();
+        this.boardTitle = boardResponseDto.getBoardTitle();
+        this.boardStatus = boardResponseDto.isBoardStatus();
+        this.boardType = boardResponseDto.getBoardType();
+        this.boardContent = boardResponseDto.getBoardContent();
+        this.commentCnt = boardResponseDto.getCommentCnt();
+        this.viewCnt = boardResponseDto.getViewCnt();
+        this.likeCnt = boardResponseDto.getLikeCnt();
+        this.imageYn = boardResponseDto.isImageYn();
+        this.boardCreatedDate = boardResponseDto.getBoardCreatedDate();
+        this.userSeq = boardResponseDto.getUserSeq();
+    }
+}
---
+++ src/main/java/com/example/boardproject/dto/LoginDto.java
@@ -0,0 +1,16 @@
+package com.example.boardproject.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginDto {
+
+    private String userId;
+    private String userPwd;
+
+}
